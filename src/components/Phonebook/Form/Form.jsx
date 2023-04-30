cf35682ace3bc9be05f4700ac6c7fde252417f44
Serhii <schenyavsky@gmail.com>
2023-04-30 13:21:55 +0200

    update

--- src/components/Phonebook/Form/Form.jsx
+++ src/components/Phonebook/Form/Form.jsx
@@ -38,6 +38,7 @@ const Form = ({ onSubmit }) => {
             onChange={handleChange}
             type="text"
             name="name"
+            autoComplete="off"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
@@ -49,6 +50,7 @@ const Form = ({ onSubmit }) => {
             onChange={handleChange}
             type="tel"
             name="number"
+            autoComplete="off"
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
